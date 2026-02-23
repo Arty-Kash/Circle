e039600ff52ddb482ad24519d613d989ff69c9e8
Arty Kash <33560795+Arty-Kash@users.noreply.github.com>
2026-02-23 09:57:14 +0900

    Pythonの最後のループの説明を追加

--- circle.py
+++ circle.py
@@ -58,4 +58,28 @@ public_url = ngrok.connect(8000)
 print(f"JavaScriptに貼り付けるURLはこれです: {public_url.public_url}/draw_circle")
 
 nest_asyncio.apply()
-uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# 修正前
+# uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# この1行だけだと，エラー（RuntimeError: asyncio.run() cannot be called from a running event loop）が発生．
+# これは、Google Colab特有の現象で，Colab自体が既にPythonを動かすための「ループ（イベントループ）」を持っているため、
+# その中でさらに新しくループを作ろうとすると「もう動いてるよ！」と怒られてしまうから。
+
+
+
+# 修正後（Colab用）
+import asyncio
+config = uvicorn.Config(app, host="0.0.0.0", port=8000)
+server = uvicorn.Server(config)
+await server.serve()
+
+# 上記修正で直る理由：
+# Colabのセルは await（待機）という命令を直接受け付けることができる特殊な環境なので，
+# uvicorn.run という「全部おまかせ」の命令ではなく、await server.serve() という
+# 「今のループを使ってサーバーを開始してね」という丁寧な命令に変えることで、エラーを回避できる。
+
+# 実行した後の状態について
+# この修正をして実行すると、セルがずっと「実行中（ぐるぐる）」の状態になり，それで正解！
+# 実行中: サーバーが起きていて、JavaScriptからのリクエストを待っている状態。
+# 停止（再生ボタンが戻る）: サーバーが寝てしまった状態。通信できない。
